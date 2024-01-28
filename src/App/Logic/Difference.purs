8fbdc0568b9ed70e942341f0958f66709df9cc3e
Marek Bauer <marekbauer07@gmail.com>
2024-01-28 18:42:14 +0100

    added logic for board differances

---
+++ src/App/Logic/Difference.purs
@@ -0,0 +1,46 @@
+module App.Logic.Difference
+  ( emptyDiffrances
+  , filledDifferances
+  )
+  where
+
+import Prelude
+
+import App.Data.Sudoku.Board (Board, Position, peekAt)
+import App.Data.Sudoku.Field (Field(..), Value(..), fieldToValue)
+import App.Logic.BoardFolds (foldBoard)
+import App.Utils.Partial (runPartial)
+import Data.Array (cons)
+import Data.Maybe (Maybe)
+import Data.Tuple (Tuple(..))
+
+-- | Check for differances up to type of entry
+--   Empty field matches everything 
+filledDifferances' :: Partial => Board -> Board -> Array Position
+filledDifferances' board board' = foldBoard checkDiff [] board
+  where
+    checkDiff :: Array Position -> Position -> Field -> Array Position
+    checkDiff prev pos field = case (Tuple field (peekAt pos board')) of 
+      Tuple Empty _ -> prev
+      Tuple _ Empty -> prev
+      Tuple x y | fieldToValue x == fieldToValue y -> prev
+      _ -> cons pos prev
+
+-- | Check for differances up to type of entry
+--   Empty field matches everything 
+filledDifferances :: Board -> Board -> Maybe (Array Position)
+filledDifferances b b' = runPartial (filledDifferances' b) b'
+
+-- | Findes all places that are empty, but solutuon have them filled 
+emptyDiffrances' :: Partial => Board -> Board -> Array (Tuple Position Value)
+emptyDiffrances' board solution = foldBoard checkEmpty [] board
+  where
+    checkEmpty :: Array (Tuple Position Value) -> Position -> Field -> Array (Tuple Position Value)
+    checkEmpty prev pos field = case (Tuple field (peekAt pos solution)) of 
+      Tuple Empty Empty -> prev
+      Tuple Empty x -> cons (Tuple pos $ fieldToValue x) prev
+      _ -> prev
+
+-- | Findes all places that are empty, but solutuon have them filled 
+emptyDiffrances :: Board -> Board -> Maybe (Array (Tuple Position Value))
+emptyDiffrances board solution = runPartial (emptyDiffrances' board) solution
