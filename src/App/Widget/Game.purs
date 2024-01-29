e08da7495821b17d6539ed7e2aa7954df302bbf3
Marek Bauer <marekbauer07@gmail.com>
2024-01-29 19:25:51 +0100

    added abstract game component

---
+++ src/App/Widget/Game.purs
@@ -0,0 +1,135 @@
+module App.Widget.Game
+  ( Action(..)
+  , MandatoryActions(..)
+  , State
+  , mkGameComponent
+  )
+  where
+
+import Prelude
+
+import App.Data.Sudoku.Board (Board, Position, Size, peekAt, setAt)
+import App.Data.Sudoku.Error (Error, GameState(..))
+import App.Data.Sudoku.Field (Value(..), fieldToValue, valueToUserInput)
+import App.Utils.Partial (runPartial)
+import App.Widget.Board as BoardWidget
+import App.Widget.Keyboard as KeyboardWidget
+import Data.Maybe (Maybe(..), fromMaybe, isNothing)
+import Data.Tuple (Tuple(..), fst, snd)
+import Effect.Aff.Class (class MonadAff)
+import Effect.Class.Console (error)
+import Halogen (ClassName(..))
+import Halogen as H
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
+import Type.Proxy (Proxy(..))
+
+type State r = 
+  { board       :: Board 
+  , selectedPos :: Maybe Position
+  , gameState   :: GameState
+  , freeze      :: Boolean
+  | r
+  }
+
+data Action i c
+  = HandleBoard BoardWidget.Output
+  | HandleKeyboard KeyboardWidget.Output
+  | Mandatory (MandatoryActions i)
+  | Custom c
+
+data MandatoryActions i
+  = BoardUpdated Board
+  | Solve 
+  | Hint
+  | Refresh i
+  | Final
+
+type Slots = 
+  ( board    :: H.Slot BoardWidget.Query BoardWidget.Output Unit
+  , keyboard :: forall q. H.Slot q KeyboardWidget.Output Unit
+  )
+
+_board = Proxy :: Proxy "board"
+_keyboard = Proxy :: Proxy "keyboard"
+
+
+mkGameComponent :: forall q m i o r c. MonadAff m 
+  => (i -> State r)
+  -> (MandatoryActions i -> H.HalogenM (State r) (Action i c) Slots o m Unit)
+  -> (c -> H.HalogenM (State r) (Action i c) Slots o m Unit)
+  -> H.Component q i o m
+mkGameComponent prepareInput handleMandatory handleCustom =
+  H.mkComponent
+    { initialState: prepareInput
+    , render
+    , eval: H.mkEval H.defaultEval { handleAction = handleAction handleMandatory handleCustom
+                                   , receive = Just <<< Mandatory <<< Refresh
+                                   , finalize = Just $ Mandatory $ Final
+                                   }
+    }
+
+render :: forall m i c r. State r -> H.ComponentHTML (Action i c) Slots m
+render state =
+  HH.div [ HP.class_ $ ClassName "game" ]
+    [ HH.slot _board unit BoardWidget.component { board: state.board, errors, solved } HandleBoard
+    , HH.div [ HP.class_ $ ClassName $ if isNothing state.selectedPos then "keyboard-hidden" else "keyboard-show" ]
+        [ HH.slot _keyboard unit KeyboardWidget.component { selected: selectedValue, size } HandleKeyboard ]
+    , HH.div [ HP.class_ $ ClassName "help-btns" ]
+      [ HH.button 
+          [ HE.onClick \_ -> Mandatory $ Solve, HP.class_ $ ClassName "solve-btn", HP.disabled solved ] 
+          [ HH.text "Solve this sudoku" ]
+      , HH.button 
+          [ HE.onClick \_ -> Mandatory $ Hint, HP.class_ $ ClassName "solve-btn", HP.disabled solved ] 
+          [ HH.text "Get hint" ]
+      ]
+    ]
+  where
+    gameState :: Tuple (Array Error) Boolean
+    gameState = case state.gameState of 
+      Incomplite err -> Tuple err false
+      Complite -> Tuple [] true
+
+    errors = fst gameState
+    solved = snd gameState
+
+    size :: Size
+    size = state.board.size
+
+    selectedValue :: Value 
+    selectedValue = fromMaybe (Value 0) $ do
+      pos <- state.selectedPos
+      field <- runPartial (peekAt pos) state.board
+      pure $ fieldToValue field
+
+handleAction :: forall c i r o m. MonadAff m 
+  => (MandatoryActions i -> H.HalogenM (State r) (Action i c) Slots o m Unit)
+  -> (c -> H.HalogenM (State r) (Action i c) Slots o m Unit) 
+  -> Action i c
+  -> H.HalogenM (State r) (Action i c) Slots o m Unit
+handleAction handleMandatory handleCustom = case _ of
+  HandleBoard msg -> case msg of 
+    BoardWidget.ValueInserted pos val -> do
+      { board } <- H.get
+      let mNewBoard = runPartial (setAt pos $ valueToUserInput val) board
+      case mNewBoard of 
+        Just b -> do
+          H.modify_ $ \s -> s { board = b }
+          handleAction handleMandatory handleCustom $ Mandatory $ BoardUpdated b
+        Nothing -> error "Could not updated board"
+    BoardWidget.SelectionChanged pos -> 
+      H.modify_ $ \s -> s { selectedPos = Just pos }
+  HandleKeyboard (KeyboardWidget.Selected val) -> do
+    { board, selectedPos } <- H.get
+    let 
+      mNewBoard = do 
+        pos <- selectedPos
+        runPartial (setAt pos $ valueToUserInput val) board
+    case mNewBoard of 
+      Just b -> do
+        H.modify_ $ \s -> s { board = b }
+        handleAction handleMandatory handleCustom $ Mandatory $ BoardUpdated b
+      Nothing -> error "Could not updated board"
+  Mandatory action -> handleMandatory action
+  Custom action -> handleCustom action
